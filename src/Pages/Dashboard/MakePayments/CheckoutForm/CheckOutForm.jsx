27044502b2d433e262b3aa1a8712126132450977
Ajmain Fayek <ajmainfayek733@gmail.com>
2025-01-22 23:25:43 +0600

    Payment complete

---
+++ src/Pages/Dashboard/MakePayments/CheckoutForm/CheckOutForm.jsx
@@ -0,0 +1,151 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useEffect, useState } from "react";
+import useAxios from "../../../../Hooks/useAxios";
+import useAuthContext from "../../../../Hooks/useAuthContext";
+import { Slide, toast } from "react-toastify";
+import { formatISO } from "date-fns";
+
+const CheckOutForm = ({ paymentData, setShowModal }) => {
+    const { user } = useAuthContext();
+    const axiosFetch = useAxios();
+    const stripe = useStripe();
+    const elements = useElements();
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [clientSecret, setClientSecret] = useState("");
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+
+        const rentAmount = paymentData?.rent;
+        if (!rentAmount || isNaN(rentAmount)) {
+            console.log("Invalid rent value on client side:", rentAmount);
+            return;
+        }
+
+        axiosFetch
+            .post("/create-payment-intent", {
+                ...paymentData,
+                token,
+                currency: "usd",
+            })
+            .then((res) => {
+                setClientSecret(res?.data?.clientSecret);
+            })
+            .catch((error) => {
+                console.error(
+                    "Error creating payment intent:",
+                    error.response.data
+                );
+            });
+    }, [paymentData, axiosFetch]);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        setErrorMessage(null);
+
+        if (!elements || !stripe || !paymentData) {
+            return;
+        }
+
+        const cardElement = elements.getElement(CardElement);
+
+        if (!cardElement) {
+            return;
+        }
+
+        const { error } = await stripe.createPaymentMethod({
+            type: "card",
+            card: cardElement,
+        });
+
+        if (error) {
+            console.log(error);
+            setErrorMessage(error.message);
+            return;
+        } else {
+            setErrorMessage(null);
+        }
+
+        // confirm payment
+        const { paymentIntent, error: paymentError } =
+            await stripe.confirmCardPayment(clientSecret, {
+                payment_method: {
+                    card: cardElement,
+                    billing_details: {
+                        name: user?.displayName,
+                        email: user?.email,
+                    },
+                },
+            });
+
+        if (paymentError) {
+            console.error("[Payment Error]", paymentError);
+            setErrorMessage(paymentError.message);
+        } else {
+            if (paymentIntent.status === "succeeded") {
+                // save payment to database
+                const token = localStorage.getItem("token");
+
+                const transaction = {
+                    ...paymentData,
+                    transactionId: paymentIntent?.id,
+                    userId: user?._id,
+                    date: formatISO(paymentIntent?.created * 1000),
+                };
+
+                const { data } = await axiosFetch.post("/save/transaction", {
+                    token: token,
+                    transaction: transaction,
+                });
+                setShowModal((prev) => !prev);
+                if (data?.insertedId) {
+                    toast.success("Payment Succeeded", {
+                        pposition: "top-right",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "light",
+                        transition: Slide,
+                    });
+                }
+            }
+        }
+    };
+
+    console.log(paymentData);
+    return (
+        <form className="space-y-4" onSubmit={handleSubmit}>
+            <div>
+                <span>
+                    Payment of {paymentData?.month} {paymentData?.rent}
+                    {" BDT "}
+                </span>
+            </div>
+            <span className="p-1 bg-red-50 block border border-red-300 text-red-700 text-xs">
+                Ex. Card No: 4242424242424242 <br /> cvc - any 3 digit number{" "}
+                <br /> date: any future date <br /> zip code: any 5 digit number
+            </span>
+            <input
+                type="text"
+                className="border w-full p-2 focus:outline-none"
+                placeholder="Coupon (Optional)"
+            />
+            <CardElement className="p-2 border border-gray-300" />
+            {errorMessage && (
+                <div className="text-red-600 text-xs">{errorMessage}</div>
+            )}
+            <button
+                className="bg-[#002] text-white px-8 p-2 mt-4 btn-sm btn rounded-none hover:bg-[#004]"
+                type="submit"
+                disabled={!stripe || !elements || !paymentData}
+            >
+                Pay
+            </button>
+        </form>
+    );
+};
+
+export default CheckOutForm;
--- src/Pages/Dashboard/MakePayments/MakePayment.jsx
+++ src/Pages/Dashboard/MakePayments/MakePayment.jsx
@@ -1,5 +1,183 @@
+import { useEffect, useState } from "react";
+import useAuthContext from "../../../Hooks/useAuthContext";
+import useAxios from "../../../Hooks/useAxios";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckOutForm from "./CheckoutForm/CheckOutForm";
+
+// TODO - Add your Stripe Public Key
+const stripePromise = loadStripe(import.meta.env.VITE_stripe_public_key);
+
 const MakePayment = () => {
-    return <div>MakePayment</div>;
+    const { user } = useAuthContext();
+    const axiosFetch = useAxios();
+    const [agreements, setAgreements] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    const [paymentData, setPaymentData] = useState([]);
+
+    const fetchAgreements = async () => {
+        try {
+            const token = localStorage.getItem("token");
+            const { data } = await axiosFetch.post("/member/agreements", {
+                token,
+                tenantEmail: user?.email,
+                tenant_id: user?._id,
+            });
+            const { result } = data;
+            const { apartmentDetails } = result;
+            setAgreements(apartmentDetails || []);
+        } catch (error) {
+            console.log("Failed to fetch Agreements Request:", error);
+        }
+    };
+
+    useEffect(() => {
+        if (user?.role === "member") {
+            fetchAgreements();
+        }
+    }, [user]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const data = Object.fromEntries(formData.entries());
+        data["userId"] = user?._id;
+        setPaymentData(data || []);
+        setShowModal((prev) => !prev);
+    };
+
+    if (agreements.length === 0) {
+        return (
+            <div className="text-center flex items-center loading_screen">
+                <span className="loading mx-auto text-center loading-ring loading-lg"></span>
+            </div>
+        );
+    }
+    return (
+        <div>
+            <h1 className="text-2xl font-semibold text-center p-2 shadow-lg mb-6 border">
+                Pay Rent
+            </h1>
+            <p className="p-1 text-red-700 text-xs">
+                {" "}
+                <span className="font-semibold">Note:</span> Please select the
+                month for rent payment.
+            </p>
+            <form
+                onSubmit={handleSubmit}
+                className="flex flex-col gap-3 border p-4 shadow-lg bg-base-200"
+            >
+                <div className="flex flex-col">
+                    <label className="font-semibold">Email: </label>
+                    <input
+                        className="border p-2 focus:outline-none border-gray-300 cursor-not-allowed"
+                        type="email"
+                        name="email"
+                        defaultValue={user?.email}
+                        readOnly
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label className="font-semibold">Floor: </label>
+                    <input
+                        className="border p-2 focus:outline-none border-gray-300 cursor-not-allowed"
+                        type="text"
+                        name="floor"
+                        defaultValue={agreements?.floorNo}
+                        readOnly
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label className="font-semibold">Block Name: </label>
+                    <input
+                        className="border p-2 focus:outline-none border-gray-300 cursor-not-allowed"
+                        type="text"
+                        name="blockName"
+                        defaultValue={agreements?.blockName}
+                        readOnly
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label className="font-semibold">
+                        Apartment No/Room No:{" "}
+                    </label>
+                    <input
+                        className="border p-2 focus:outline-none border-gray-300 cursor-not-allowed"
+                        type="text"
+                        name="apartmentNo"
+                        defaultValue={agreements?.apartmentNo}
+                        readOnly
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label className="font-semibold">Rent: </label>
+                    <input
+                        className="border p-2 focus:outline-none border-gray-300 cursor-not-allowed"
+                        type="text"
+                        name="rent"
+                        defaultValue={agreements?.rent}
+                        readOnly
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label className="font-semibold">Month: </label>
+                    <select
+                        className="border p-2 focus:outline-none border-gray-300 cursor-pointer"
+                        name="month"
+                        defaultValue={new Date().toLocaleString("default", {
+                            month: "long",
+                        })}
+                        required
+                    >
+                        <option value="">Select Month</option>
+                        <option value="January">January</option>
+                        <option value="February">February</option>
+                        <option value="March">March</option>
+                        <option value="April">April</option>
+                        <option value="May">May</option>
+                        <option value="June">June</option>
+                        <option value="July">July</option>
+                        <option value="August">August</option>
+                        <option value="September">September</option>
+                        <option value="October">October</option>
+                        <option value="November">November</option>
+                        <option value="December">December</option>
+                    </select>
+                </div>
+                <button
+                    type="submit"
+                    className="bg-[#002] text-white btn-wide p-2 mt-4 btn rounded-none hover:bg-[#004]"
+                >
+                    Pay
+                </button>
+            </form>
+            {/* Payment Modal */}
+            <dialog
+                id="my_modal_3"
+                className={`modal ${showModal ? "modal-open" : ""}`}
+            >
+                <div className="modal-box rounded-none">
+                    <form onSubmit={(e) => e.preventDefault()} method="dialog">
+                        {/* if there is a button in form, it will close the modal */}
+                        <button
+                            onClick={() => setShowModal((prev) => !prev)}
+                            className="btn text-red-600 btn-sm btn-circle btn-ghost absolute right-2 top-2"
+                        >
+                            ✕
+                        </button>
+                    </form>
+                    <div className="py-4">
+                        <Elements stripe={stripePromise}>
+                            <CheckOutForm
+                                paymentData={paymentData}
+                                setShowModal={setShowModal}
+                            />
+                        </Elements>
+                    </div>
+                </div>
+            </dialog>
+        </div>
+    );
 };
 
 export default MakePayment;
